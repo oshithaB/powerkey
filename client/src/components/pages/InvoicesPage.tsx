aae2be1ea49474d9c56ae5726d67b4efb4a8b803
Ramitha Heshan <ramithacampus@gmail.com>
2025-08-26 10:54:01 +0530

    invoice table update

--- client/src/components/pages/InvoicesPage.tsx
+++ client/src/components/pages/InvoicesPage.tsx
@@ -572,7 +572,7 @@ export default function InvoicesPage() {
                   Customer
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Employee
+                  Sales Person
                 </th>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Date

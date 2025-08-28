d12efa04974b4313c6c4b38d9ff7e9d9b24ef575
Ramitha Heshan <ramithacampus@gmail.com>
2025-08-29 00:55:06 +0530

    commission report is done

--- server/routes/reports.js
+++ server/routes/reports.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const reportController = require('../controllers/reports/profit&lossreport_controller');
+const commissionReportController = require('../controllers/reports/commissionreport_controller');
 const verifyToken = require('../middleware/verifyToken');
 const authorizedRoles = require('../middleware/authorized-roles');
 
 const {
     getProfitAndLossData,
-    getMonthlyProfitAndLoss
+    getMonthlyProfitAndLoss,
 } = reportController;
 
+const {
+    getCommissionReport
+} = commissionReportController;
+
+// Profit and Loss Report Routes
 router.get(
     '/profit-and-loss/:company_id',
     verifyToken,
@@ -21,6 +27,14 @@ router.get(
     verifyToken,
     authorizedRoles(['admin', 'manager', 'accountant']),
     getMonthlyProfitAndLoss
+);
+
+// Commission Report Route
+router.get(
+    '/commission-report',
+    verifyToken,
+    authorizedRoles(['admin', 'manager']),
+    getCommissionReport
 );
 
 module.exports = router;
